34a92cd4de4f2e4adc46254cddaf3f617df50de0
Josefine Magnussen <josefine.magnussen@hotmail.com>
2019-09-22 00:02:07 +0200

    fjernet
    Fjerna lorem og la tid timeline tekst

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main">
     <h1>About Josefine</h1>    
-    <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi, necessitatibus neque aspernatur corrupti quos dolorum voluptate laboriosam quibusdam veniam sint, fugiat libero consequatur deserunt delectus quidem ipsa eius! Totam, dolorum!</p>    
+    <p> - Timeline</p>    
     
     <p>If you want to contact me just send me a mail!</p>
     </div>
