cf540eea3669538a30c11f4dfa32fa025dc9e130
Josefine Holth <josefine.magnussen@hotmail.com>
2020-11-27 14:18:37 +0100

    new layout and content

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,116 +1,285 @@
 <template>
-  <main class="container">
-    <div class="grid-item">
-      <img class="image" src="../assets/images/Josefine Magnussen.jpg" alt="Josefine Magnussen" />
-    </div>
-    <div class="grid-text">
-      <p>Mitt navn er Josefine.</p>
-      <p>
-        Jeg er en IT-Utvikler fra Larvik. For det meste bruker jeg tiden
-        foran pc der jeg koder for det mest frontend men også en del backend.
-        Jeg er altså en full-stack utvikler som er kreativ, nyskjerig og brenner
-        for faget sitt.
-      </p>
-      <div class="vl list">
-        <ul style="float: left;">
-          <h3>Frontend</h3>
+  <div>
+    <section>
+      <div class="container">
+        <div></div>
+        <div class="content">
+          <div>
+            <img src="../assets/images/JosefineHolth.png" alt="" srcset="" />
+          </div>
+          <div class="text">
+            <h1>Josefine Holth</h1>
+            <h1>Web Developer</h1>
+          </div>
+        </div>
+        <div></div>
+      </div>
+
+      <div class="sections">
+        <h1>Hello World!</h1>
+        <p>
+          Mitt navn er Josefine. Jeg er en webutvikler. Jeg bruker tiden min på
+          personlige prosjekter og lærer meg mer om PHP, Javascript, HTML og
+          CSS.
+        </p>
+        <p>
+          På fritiden liker jeg og holde på med fotografering, tilbringe tid ute
+          i naturen og selvfølgelig spilling.
+        </p>
+        <p>
+          Hvis du har en idé om et nettsted eller ditt nåværende nettsted
+          trenger en oppgradering så ta gjerne kontakt med meg så kan vi se hva
+          vi kan få til. Eller vis du vil snakke om jobb muligheter.
+        </p>
+        <div class="center">
+          <button @click="aboutMe()">Les Mere</button>
+          <a href="mailto:YourMail@Mail.com">Kontakt Meg</a>
+        </div>
+      </div>
+    </section>
+
+    <section class="sectionsWithColor" style="background-color: #ebe8e1">
+      <h1>Mine Skills</h1>
+      <div class="skillContainer">
+        <ul>
+          <li>PHP</li>
+          <li>Javascript</li>
           <li>HTML</li>
           <li>CSS</li>
-          <li>JavaScript</li>
-          <li>JSON</li>
-          <li>Vue.js</li>
-        </ul>
-
-        <ul style="float: left;">
-          <h3>Backend</h3>
-          <li>C#</li>
-          <li>ASP.Net core</li>
-          <li>PHP</li>
-          <li>API i php, asp og json</li>
-          <li>SQL</li>
+          <li>Git </li>
         </ul>
       </div>
-      <div class="container-text">
-        <div class="contact-button">
-          <a href="mailto:josefine.magnussen@hotmail.com" target="_top">Kontakt meg gjerne på mail</a>
+</section>
+
+    <section style="display:none;">
+      <h1 class="prosject__h2">Prosjects</h1>
+      <div>
+        <div class="prosject__container">
+            <h3>Project 1</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut saepe
+              dolor laboriosam cum, placeat voluptatum fugiat itaque non
+              expedita ipsa ducimus dicta! Doloremque repellat delectus odio
+              ullam quidem perspiciatis sunt.
+            </p>
+          </div>
+          <div>
+            <h3>Project 2</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut saepe
+              dolor laboriosam cum, placeat voluptatum fugiat itaque non
+              expedita ipsa ducimus dicta! Doloremque repellat delectus odio
+              ullam quidem perspiciatis sunt.
+            </p>
+          </div>
+          <div>
+            <h3>Project 3</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut saepe
+              dolor laboriosam cum, placeat voluptatum fugiat itaque non
+              expedita ipsa ducimus dicta! Doloremque repellat delectus odio
+              ullam quidem perspiciatis sunt.
+            </p>
+          </div>
         </div>
-<h2 class="changes">Changes are coming!</h2>
+      <div class="center">
+        <button class="readMore">ReadMore</button>
       </div>
-      <div class="footer"></div>
-    </div>
-  </main>
+    </section>
+  </div>
 </template>
+<script>
+import axios from "axios";
 
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  //  mounted () {
+  //   axios
+  //     .get('data.json')
+  //     .then((response) => {
+  //       this.projects = response.data
+  //       console.log()
+  //       })
+  // },
+
+  methods: {
+    aboutMe() {
+      this.$router.push("/about");
+    },
+  },
+};
+</script>
+<!-- <style scoped>
+@import "../assets/Style/style.css";
+</style> -->
 <style scoped>
-.changes {
-  padding-top: 50px;
-  text-align: center;
-}
-main {
-  padding-bottom: 20px;
-}
-.container {
-  min-height: 300px;
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-  grid-template-areas: "image content";
-  margin-bottom: 150px;
-}
-
-.grid-item {
-  grid-area: image;
-}
-
-.grid-text {
-  grid-area: content;
-  color: #2b1621;
-  height: 300px;
-  padding-top: 15%;
-  padding-right: 60px;
-  margin-bottom: 200px;
-}
-.container-text {
-  padding-top: 30px;
-}
 
 img {
-  padding: 15%;
   display: block;
   margin-left: auto;
   margin-right: auto;
-  width: 70%;
+  width: 80%;
 }
 
-p {
-  line-height: 35px;
+h1 {
+  font-size: 1.5rem;
+  font-family: "Montserrat", sans-serif;
+  color: #693433;
+  text-transform: uppercase;
+  text-align: center;
 }
-h2 {
-  padding-top: 5%;
+.container {
+
+  padding-top: 15%;
+  display: grid;
 }
-ul {
-  list-style-type: none;
+
+.skillContainer ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-align: center;
 }
-li {
-  padding-top: 10px;
+
+.skillContainer li {
+  padding: 2%;
 }
-.vl {
-  border-left: 2px solid #402131;
-  height: 300px;
+
+.text {
+  padding-top: 9%;
+}
+
+.text > h1 {
+  font-family: "Modak", cursive;
+  font-size: 10vw;
+  line-height: 7vw;
+  margin: 5px;
+  
+  text-align: center;
+  font-weight: lighter;
+  color: #693433;
+}
+
+.sections {
+  margin: 25% 10%;
+  font-size: 1rem;
+}
+
+.sectionsWithColor {
+  padding: 10% 10%;
+  font-size: 1rem;
+}
+
+.center > button,
+a {
+  text-decoration: none;
+  color: black;
+  margin-top: 15%;
+  font-size: 16px;
+  padding: 15px 30px;
+  border: none;
+  border-radius: 33px;
+  background: #ffffff;
+  box-shadow: 14px 14px 19px #e6e6e6, -14px -14px 19px #ffffff;
+}
+
+.center > button:hover,
+a:hover{
+  font-weight: bold;
+  color: #693433;
+}
+
+.center {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 0px 30px 0px;
 }
 
 @media only screen and (max-width: 800px) {
-  .footer {
-    padding-bottom: 150px;
+.readMore {
+  font-size: 16px;
+  padding: 15px 32px;
+  border: none;
+  border-radius: 33px;
+  background: #ffffff;
+  box-shadow: 14px 14px 19px #e6e6e6, -14px -14px 19px #ffffff;
+}
+.readMore:hover {
+  font-weight: bold;
+}
+.prosject__h2 {
+  text-align: center;
+  padding-top: 5%;
+  color: #7b6950;
+}}
+@media only screen and (min-width: 400px) {
+  img{
+    width: 90%;
   }
-  body {
-    background-color: lightblue;
+  .sectionsWithColor ul{
+    display: grid;
+    grid-template-columns: 50% 50%;
+    column-gap: 10px;
   }
+    .text > h1{
+    padding: 5% 0 0 0;
+        font-size: 10vw;
+    line-height: 5vw;
+  }
+
+  .sections{
+    margin: 20% 10%;
+  }
+}
+@media only screen and (min-width: 800px) {
+  h1{
+    font-size: 2rem;
+  }
+  p{
+    font-size: 1.2rem
+  } 
   .container {
     margin-bottom: 150px;
     min-height: 300px;
     width: 100%;
+    grid-template-columns: 10% 80% 10%;
+  } 
+  .content {
+    display: grid;
+    grid-template-columns: 60% 40%;
+    column-gap: 10px;
+  }
+  .sections {
+    margin: 10% 10%;
+    font-size: 1rem;
+  }
+  .text {
+    padding-top: 30%;
+  }
+  .text > h1 {
+    font-size: 4vw;
+    line-height: 3vw;
+  }
+  .aboutMe {
+    margin: 15% 13%;
+  }
+  .skillContainer ul{
+    display: grid;
+    grid-template-columns: 33% 33% 33%;
+    column-gap: 10px;
+    font-size: 1.5rem;
+  }
+  .skillContainer ul li {
+    padding-top: 20px;
+    list-style: none;
+    flex: 0 0 33%;
+  }
+  .prosject__container {
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: 1fr;
@@ -139,6 +308,17 @@ li {
   .vl {
     border-left: 2px solid #402131;
     height: 300px;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  .prosject__container > div {
+    padding: 7%;
+  }
+  .prosject__container > div > h3 {
+    color: #7b6950;
+  }
+  .prosject__container > div > p {
+    font-size: 18px;
+    color: darkslategray;
   }
 }
 </style>
