fa97f516181f47248aef5512038a21ba8456a869
Josefine Holth <josefine.magnussen@hotmail.com>
2020-12-16 10:25:45 +0100

    redon the about me page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,112 +1,90 @@
 <template>
-  <main class="container">
-    <div class="grid-item">
-      <img class="image" src="../assets/images/IMG_0846.jpg" alt="Josefine Magnussen" />
-    </div>
-    <div class="grid-text">
-      <h2>Om Josefine</h2>
-      <!-- <p>timeline</p> -->
-      <p>
-        I 2018 startet jeg på GET Academy i Larvik på kurset Start It for og lære meg programmering.
-        Der programmerte jeg mest HTML, CSS og Javascript.
-      </p>
-      <p>
-        Fra August 2018 til Juni 2019 ble jeg tilbud et års studie på GET Academy.
-        Der jobbet jeg med både PHP, C#, ASP .Net core, SQL (PostgreSQL, MySQL og SQL Server).
-        Jeg har også jobbet litt med Bootstrap og Vuetify.
-        Rammeverk er jeg mest vant til å bruke Vue.js.
-      </p>
-      <p>
-        For det meste har jeg brukt PHP og ASP.Net core til å lage RESTapiet og bruke
-        Vue.js som frontend rammeverk.
-      </p>
-      <p>
-        Grunnen til at jeg liker IT-Utvikling er at det er spennende, nytenkende og ikke mins så lærer man seg noe nytt vær dag.
-        Jeg har alltid vært nysgjerrig og interessert i tek.
-        Men mest av alt så for jeg jobbe med noe jeg liker og brenner for.
-      </p>
-      <a href="mailto:josefine.magnussen@hotmail.com" target="_top">Kontakt meg gjerne på mail.</a>
-      <!-- <router-link class="link"  to="/projects"> Mine prosjekter</router-link> -->
-    </div>
-  </main>
+  <div>
+    <section>
+      <div class="container">
+        <div></div>
+        <div>
+          <div class="imgContainer">
+            <img src="../assets/images/2020-12-16-072821387.jpg" alt="" srcset="" />
+          </div>
+          <div class="textContainer">
+            <h2>Josefine Holth</h2>
+            <p>
+              I 2018 startet jeg på GET Academy i Larvik på kurset Start It for og lære
+              meg programmering. Der programmerte jeg mest HTML, CSS og Javascript.
+            </p>
+            <p>
+              Fra August 2018 til Juni 2019 ble jeg tilbud et års studie på GET Academy.
+              Der jobbet jeg med både PHP, C#, ASP .Net core, SQL (PostgreSQL, MySQL og
+              SQL Server). Jeg har også jobbet litt med Bootstrap og Vuetify. Rammeverk er
+              jeg mest vant til å bruke Vue.js.
+            </p>
+            <p>
+              For det meste har jeg brukt PHP og ASP.Net core til å lage RESTapiet og
+              bruke Vue.js som frontend rammeverk.
+            </p>
+            <p>
+              Grunnen til at jeg liker IT-Utvikling er at det er spennende, nytenkende og
+              ikke mins så lærer man seg noe nytt vær dag. Jeg har alltid vært nysgjerrig
+              og interessert i tek. Men mest av alt så for jeg jobbe med noe jeg liker og
+              brenner for.
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
-
-a{
-  color: #402131;
-  font-size: 20px;
-}
-a:hover{
-  color: grey;
-  font-size: 20px;
+img {
+  width: 90%;
 }
 .container {
-  min-height: 300px;
-  width: 100%;
+  margin: 3% 10% 3% 10%;
+  padding-top: 15%;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-  grid-template-areas: "image content";
-  margin-bottom: 150px;
+}
+.imgContainer {
+  display: flex;
+  justify-content: center;
+}
+.textContainer h2 {
+  text-align: center;
+  margin: 4rem 0 4rem 0;
+  font-size: 2.2rem;
+  font-family: "Nixie One", cursive;
+  color: #1b5e36;
+}
+.textContainer > p {
+  font-size: 16px;
+}
+.textContainer {
+  margin-bottom: 8%;
 }
 
-.grid-item {
-  grid-area: image;
-}
-
-.grid-text {
-  grid-area: content;
-  color: #2b1621;
-  height: 300px;
-  padding-right: 60px;
-
-}
-img {
-   padding: 3%;
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 70%;
-}
- a{
-    padding-bottom: 100px;
-    text-decoration: none;
-    font-weight: bold;
- }
- a:hover{
-   color: purple;
- }
-@media only screen and (max-width: 800px) {
+@media only screen and (min-width: 400px) {
   .container {
-    min-height: 300px;
+    margin: 3% 10% 3% 10%;
+  }
+}
+@media only screen and (min-width: 800px) {
+  img {
     width: 100%;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
-    grid-template-areas:
-      "image"
-      "content";
-       margin-bottom: 150px;
   }
-  .grid-text {
-    grid-area: content;
-    color: #2b1621;
-    height: 300px;
-    margin-bottom: 150px;
-    padding-left: 60px;
+  .textContainer {
+    margin-bottom: 8%;
   }
-  .image {
-    display: initial;
-    margin: 0;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-  }
- a{
-    padding-bottom: 100px;
- }
+.textContainer > p {
+  font-size: 20px;
+}
+  .container {
+    padding-top: 8%;
+    min-height: 300px;
+    grid-template-columns: 20% 60% 20%;
+        margin: 0 3% 0 3%;
 
+  }
 }
 </style>
