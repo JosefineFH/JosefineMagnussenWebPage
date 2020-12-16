645da954308755a4ff51fd4d64a3c8dd22879b5b
Josefine Holth <43983109+JosefineFH@users.noreply.github.com>
2020-12-16 10:34:01 +0100

    Update router.js
    fix spaces that was not found, removed blank lines

--- src/router.js
+++ src/router.js
@@ -1,30 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'main',
-        component: () => import ('./components/Main.vue')
-    }, {
-        path: '/projects',
-        Name: 'projects',
-        component: () => import ('./components/Projects.vue')
-    }, {
-        path: '/about',
-        Name: 'about',
-        component: () => import ('./components/About.vue')
-    }, {
-        path: '/contact',
-        Name: 'contact',
-        component: () => import ('./components/Contact.vue')
-    }
+  {
+    path: '/',
+    name: 'main',
+    component: () => import ('./components/Main.vue')
+  }, {
+    path: '/projects',
+    Name: 'projects',
+    component: () => import ('./components/Projects.vue')
+  }, {
+    path: '/about',
+    Name: 'about',
+    component: () => import ('./components/About.vue')
+  }, {
+    path: '/contact',
+    Name: 'contact',
+    component: () => import ('./components/Contact.vue')
+  }
 ]
 
-const router = new VueRouter({mode: 'history', base: process.env.BASE_URL, routes})
+const router = new VueRouter({ mode: 'history', base: process.env.BASE_URL, routes })
 
 export default router
-
