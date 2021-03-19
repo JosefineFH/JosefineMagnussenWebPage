d4c13f5e8131595417ea0e63d17b5228544a6a72
Josefine Holth <Josefine.Holth@hotmail.com>
2021-03-19 11:17:42 +0100

    new updates

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,24 +1,54 @@
 <template>
   <div class="prosject-container">
-  <h2>Changes are coming!</h2>
     <h1>Mine Prosjekter</h1>
+    <section class="card tablet">
+      <div>
+        <h3>Rainydays</h3>
+        <h5>Ongoing</h5>
+        <p>E - Commercial site | HTML and CSS</p>
+        <p>Dette er et skoleprosjekt som jeg holder på med</p>
+        <a class="links" href="https://github.com/JosefineFH/ecommerce-Rainyday"
+          >Code på GitHub</a
+        >
+        <br />
+        <a class="links" href="https://rainydaysecommerce.netlify.app/"
+          >Live Demo</a
+        >
+      </div>
+    </section>
+
+    <section class="card tablet">
+      <div>
+        <h3>Game Shop</h3>
+        <p>E - Commercial site | HTML, CSS og JavaScript</p>
+        <a class="links" href="https://gitlab.com/JosefineFH/gameEcommerce"
+          >Code på GitLab</a
+        >
+        <br />
+        <a class="links" href="https://ecommercegames.netlify.app/"
+          >Live Demo</a
+        >
+      </div>
+    </section>
+
     <section class="card">
-   
       <div>
         <h3>Girl Camp</h3>
-        <p>Frontend prosjekt der jeg i hovedsak kun har bruk rammeverket Vue.js.</p>
+        <p>
+          Frontend prosjekt der jeg i hovedsak kun har bruk rammeverket Vue.js.
+        </p>
         <p>Webpage med responsive design.</p>
         <a
           class="links"
           href="https://github.com/JosefineFM/Portflio/tree/master/Vue/girlcamp"
-        >Code på GitHub</a>
+          >Code på GitHub</a
+        >
         <br />
         <a class="links" href="https://techcamp.netlify.app/">Live Demo</a>
       </div>
     </section>
 
     <section class="card tablet">
-      <img class="img-tablet" src="../assets/images/vue-prod.png"/>
       <div>
         <h3>Employee Manager</h3>
         <p>Webapplikasjon med responsive design</p>
@@ -35,32 +65,14 @@
         <a
           class="links"
           href="https://github.com/JosefineFM/Portflio/tree/master/adminmanager"
-        >Code på GitHub</a>
+          >Code på GitHub</a
+        >
         <br />
         <a
           class="links"
           href="https://vue-prod-3615c.firebaseapp.com/#/login?redirect=%2F"
-        >Live Demo</a>
-      </div>
-    </section>
-
-    <section class="card">
-      <!-- <img src="../assets/images/vue-prod.png" style="width: 400px; padding-top: 15%;" /> -->
-      <div>
-        <h3>Matvaretabellen</h3>
-        <p>Dette jobber jeg med akkurat nå.</p>
-        <p>
-          <span style="font-weight: bold;">Frontend:</span>
-          HTML, CSS og rammeverket Vue.js.
-        </p>
-        <p>
-          <span style="font-weight: bold;">Backend:</span>
-          Asp.Net Core og SQL Server.
-        </p>
-        <a
-          class="links"
-          href="https://github.com/JosefineFM/Portflio/tree/master/Food%20Tabel"
-        >Code på GitHub</a>
+          >Live Demo</a
+        >
       </div>
     </section>
   </div>
@@ -68,7 +80,7 @@
 
 <style scoped>
 @import "../assets/Style/projects.css";
-h2{
+h2 {
   text-align: center;
   padding: 10%;
 }
